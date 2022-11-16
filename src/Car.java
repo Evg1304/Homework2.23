bc4e761b123b4d0202e6365cb65c6db75f572fdd
mishe <mishel1400@mail.ru>
2022-11-16 14:03:07 +0300

    Сделала ДЗ

--- src/Car.java
+++ src/Car.java
@@ -5,13 +5,32 @@ public class Car {
     String color;
     int year;
     String country;
+    public static String Car (String brand ){
+        if (brand != null){
+            brand = brand;
+        } else{
+            brand = "default";
+        }
+        return brand;
+
+}
+public static int Car1 (int year1){
+        if (year1 <= 0){
+            year1 = 2000;
+        }
+        return year1;
+}
 
     Car(String brand, String model, double engineVolume, String color, int year, String country){
+
+        if (this.brand == null){
+            this.brand = "default";
+        }
         this.brand = brand;
         this.model = model;
         this.engineVolume = engineVolume;
         this.color = color;
-        this.year = year;
+        Car1(this.year);
         this.country = country;
         System.out.println(brand + " " + year + " " + model + " " + engineVolume + " " + color + " " + country);
     }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-            Car Lada = new Car("Lada", "Granta",1.7,"желтый",2015,"Россия" );
+            Car Lada = new Car("Lada", "Granta",1.7,"желтый",-1,"Россия" );
             Car Audi = new Car("Audi", "A8",3.0,"черный",2020,"Германия" );
             Car BMW = new Car("BMW", "Z8",2.4,"красный",2018,"Южная Корея" );
             Car Kia = new Car("Kia", "Sportage",3.0,"красный",2021,"Германия" );
