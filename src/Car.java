51445fef35e613a7cceeb454e57da5a2959e227f
mishe <mishel1400@mail.ru>
2022-11-16 11:50:22 +0300

    Сделала ДЗ

---
+++ src/Car.java
@@ -0,0 +1,19 @@
+public class Car {
+    String brand;
+    String model;
+    double engineVolume;
+    String color;
+    int year;
+    String country;
+
+    Car(String brand, String model, double engineVolume, String color, int year, String country){
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+        System.out.println(brand + " " + year + " " + model + " " + engineVolume + " " + color + " " + country);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+            Car Lada = new Car("Lada", "Granta",1.7,"желтый",2015,"Россия" );
+            Car Audi = new Car("Audi", "A8",3.0,"черный",2020,"Германия" );
+            Car BMW = new Car("BMW", "Z8",2.4,"красный",2018,"Южная Корея" );
+            Car Kia = new Car("Kia", "Sportage",3.0,"красный",2021,"Германия" );
+
     }
 }
